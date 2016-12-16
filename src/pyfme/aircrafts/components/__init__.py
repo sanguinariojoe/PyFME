3c75e4a5b5b858754a9feaeabcf37f32b96be6f5
Jose Luis Cercos Pita <jlcercos@gmail.com>
2016-12-16 15:10:52 +0100

    Fixed the import model

--- src/pyfme/aircrafts/components/__init__.py
+++ src/pyfme/aircrafts/components/__init__.py
@@ -4,4 +4,6 @@ Copyright (c) AeroPython Development Team.
 Distributed under the terms of the MIT License.
 """
 
-from .aircraft import Aircraft, Propeller, Wing
+from .aircraft import Aircraft
+from .propeller import Propeller
+from .wing import Wing, Flap
